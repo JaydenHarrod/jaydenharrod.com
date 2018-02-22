1d9313a697be37a80a46b5595fe8647da8d596c7
Jayden Harrod <harrod.jayden@gmail.com>
2018-02-22 12:34:08 +1100

    Box Links Hot Fix

--- src/components/shared/Box.jsx
+++ src/components/shared/Box.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Col1 } from "./Layout";
 import styles from "../../styles/components/shared/box.scss";
 
@@ -6,9 +7,14 @@ export const Box = props => (
   <Col1>
     <div className="disable-text-on">
       <h4 className="text-overlay">{props.title}</h4>
-      <a href={props.path}>
-        <img src={props.src} className={props.class} alt={props.alt} aria-label={props.alt} />
-      </a>
+      <Link to={props.path}>
+        <img
+          src={props.src}
+          className={props.class}
+          alt={props.alt}
+          aria-label={props.alt}
+        />
+      </Link>
     </div>
   </Col1>
 );
