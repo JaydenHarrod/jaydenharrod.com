4a7ac45c98d0ffefbb1d4054356f5e7996ab1296
Jayden Harrod <harrod.jayden@gmail.com>
2018-03-12 00:53:39 +1100

    Home View Cleanup

--- src/views/Filmmaker.jsx
+++ src/views/Filmmaker.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
-import { Container, Col1 } from "../components/shared/Layout";
+import { Container, Col1, TextBox } from "../components/shared/Layout";
 import "../styles/components/filmmaker.scss";
 
 class Filmmaker extends Component {
@@ -14,7 +14,7 @@ class Filmmaker extends Component {
           <h1>Filmmaker</h1>
           <p>I'm a freelance videographer, photographer, and editor. My speciality in the industry is attention to high quality audio recording, and the use of SFX to create immersive edits. I also strategize videos that recieve high engagement on social media from my days as a YouTube partner.</p>
           <p>To discuss freelance services please email me: <a href="mailto:harrod.jayden@gmail.com">harrod.jayden@gmail.com</a></p>
-          <h3 style={{ marginTop: "32px" }}>Equipment</h3>
+          <TextBox>
           <div className="skill-container">
             <span className="skill-badge">Lumix GH5 (4K, Full HD)</span>
             <span className="skill-badge">Lumix 12-35 F/2.8</span>
@@ -27,6 +27,7 @@ class Filmmaker extends Component {
             <span className="skill-badge">Zhiyun Crane 2 Gimbal</span>
             <span className="skill-badge">Livestreaming</span>
           </div>
+          </TextBox>
           <h2 style={{ marginTop: "32px", marginBottom: "0" }}>Projects</h2>
           <div className="video-wrapper"><iframe src="https://player.vimeo.com/video/251011540" width="720" height="405" frameBorder="0" allowFullScreen></iframe></div>
           <div className="video-wrapper"><iframe src="https://player.vimeo.com/video/251011915" width="720" height="405" frameBorder="0" allowFullScreen></iframe></div>
