cb4152aa79260c4e042446fb3a0d22179a4bb34e
tbmjayden <jayden.harrod@thebetmakers.com>
2020-01-13 22:10:39 +1100

    2019

--- jh/src/components/Nineteen/Finance/index.jsx
+++ jh/src/components/Nineteen/Finance/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HeadingOne, Paragraph, Card, MetricBox } from "../../../shared";
+import { HeadingOne, Paragraph, Card, MetricBox, Row } from "../../../shared";
 import FinancePieChart from "./FinancePieChart";
 
 const Finance = () => {
@@ -10,36 +10,26 @@ const Finance = () => {
         Finance
       </HeadingOne>
       <Card header title="Expenses">
-        <MetricBox
-          vertical
-          color="rgb(36, 101, 65)"
-          title="income increased"
-          metric="38%"
-        />
-        <MetricBox
-          vertical
-          color="rgb(36, 101, 65)"
-          title="car purchased"
-          metric="$10,000"
-        />
-        <MetricBox
-          vertical
-          color="rgb(101, 36, 36)"
-          title="total spent"
-          metric="$52,000"
-        />
-        <MetricBox
-          vertical
-          color="rgb(101, 36, 36)"
-          title="monthly avg"
-          metric="$4,300"
-        />
-        <MetricBox
-          vertical
-          color="rgb(101, 36, 36)"
-          title="total change yoy"
-          metric="+57%"
-        />
+        <Row>
+          <MetricBox
+            color="rgb(36, 101, 65)"
+            title="income increase"
+            metric="38%"
+          />
+          <MetricBox
+            color="rgb(36, 101, 65)"
+            title="car purchase"
+            metric="$10K"
+          />
+        </Row>
+        <Row>
+          <MetricBox
+            color="rgb(101, 36, 36)"
+            title="total spent"
+            metric="$52K"
+          />
+          <MetricBox color="rgb(101, 36, 36)" title="yoy spent" metric="+57%" />
+        </Row>
         <Paragraph bold center pt={3} mb={0}>
           SPENDING BREAKDOWN
         </Paragraph>
